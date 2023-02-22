3e97135c19880f6f4427c6fb8e4ca637c3f691fd
Sihle Hlophe <wldrms@outlook.com>
2023-02-23 00:24:58 +0200

    models

---
+++ bootcamp/housesApp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class houseListingModel(models.Model):
+    title = models.CharField(max_length=250)
+    price = models.IntegerField()
+    square_feet = models.IntegerField()
+    no_of_bedrooms = models.IntegerField()
+    address = models.CharField(max_length=250)
+    image = models.ImageField()
+
+    # def __str__(self):
+    #   return self.title
